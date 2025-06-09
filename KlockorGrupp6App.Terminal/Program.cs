efba0cfe330788f1d95a6d48599a998166d7322c
NiklasMattsson <niklasmattson1@gmail.com>
2025-06-09 14:55:12 +0200

    Uppgift Textfil, terminal/tests

---
+++ KlockorGrupp6App.Terminal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KlockorGrupp6App.Terminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
