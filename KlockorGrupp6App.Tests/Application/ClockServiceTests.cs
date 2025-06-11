7784a268e7530aa633298733bda13e3ae3f1bf3e
Vilith <vilith44@gmail.com>
2025-06-11 13:24:35 +0200

    Added a few unit-tests with various usage. Need some löööps bröther

--- KlockorGrupp6App.Tests/Application/ClockServiceTests.cs
+++ KlockorGrupp6App.Tests/Application/ClockServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using KlockorGrupp6App.Application.Clocks.Interfaces;
 using KlockorGrupp6App.Application.Clocks.Services;
 using KlockorGrupp6App.Domain;
+using KlockorGrupp6App.Application;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -15,8 +16,8 @@ namespace KlockorGrupp6App.Tests.Application
         #region [Test Setup]
         private readonly Mock<IClockRepository> _mockClockRepository;
         private readonly Mock<IUnitOfWork> _mockUnitOfWork; // UnitOfWork still needs to be implemented        
-        private readonly ClockService _service;        
-                
+        private readonly ClockService _service;
+
         public ClockServiceTests()
         {
             _mockClockRepository = new Mock<IClockRepository>();
@@ -26,6 +27,51 @@ namespace KlockorGrupp6App.Tests.Application
 
         }
         #endregion
+
+        [Fact]
+        public void GetAll_ShouldReturnAllClocks()
+        {
+            // Arrange - Creating a list with 2 clock-objects
+            var clocks = new List<Clock>
+            {
+                new Clock { Id = 1, Brand = "Rolex", Model = "Submariner", Price = 98000m, Year = new DateTime(2022, 1, 1) },
+                new Clock { Id = 2, Brand = "Omega", Model = "Speedmaster", Price = 67000m, Year = new DateTime(2021, 1, 1) }
+            };
+
+            // Setting up the mock repository so when GetAll() is called. It will "pretend" to return the clocks list we created above.
+            _mockClockRepository.Setup(repo => repo.GetAll()).Returns(clocks.ToArray());
+
+            // Act - Calling the GetAll method on the service (Wich we have mocked to return the clocks list)
+            var result = _service.GetAll();
+
+            // Assert
+            Assert.Equal(2, result.Length); // Checking that the result has 2 clocks
+            Assert.Equal("Rolex", result[0].Brand); // Checking that the first clock's brand is "Rolex"
+        }
+
+        [Fact]
+        public void GetAll_WhenNoClocksExist_ShouldReturnEmptyArray()
+        {
+            // Arrange
+            _mockClockRepository.Setup(repo => repo.GetAll()).Returns(Array.Empty<Clock>());
+
+            // Act
+            var result = _service.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetById_WhenClockDoesNotExist_ShouldReturnNull()
+        {
+            _mockClockRepository.Setup(repo => repo.GetById(It.IsAny<int>())).Returns((Clock?)null);
+
+            var result = _service.GetById(666);
+
+            Assert.Null(result);
+        }
 
         //Still a work in progress. Need to fix Async methods and implement UnitOfWork before getting things in order.
     }
