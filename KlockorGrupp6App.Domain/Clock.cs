4a76d76729ae6c4c6e3901decdd33d9f7733cc6d
Zhodes <zhodes@users.noreply.github.com>
2025-06-12 11:47:41 +0200

    terminal projekt setup

--- KlockorGrupp6App.Domain/Clock.cs
+++ KlockorGrupp6App.Domain/Clock.cs
@@ -3,11 +3,11 @@
 public class Clock
 {
     public int Id { get; set; }
-    public string Brand { get; set; }
-    public string Model { get; set; }
+    public string Brand { get; set; } = null!;
+    public string Model { get; set; } = null!;
     public decimal Price { get; set; }
     public DateTime Year { get; set; }
 
-    public string CreatedByUserID { get; set; } 
+    public string CreatedByUserID { get; set; } = null!;
 }
 
--- KlockorGrupp6App.Terminal/Program.cs
+++ KlockorGrupp6App.Terminal/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace KlockorGrupp6App.Terminal
+﻿using KlockorGrupp6App.Application;
+using KlockorGrupp6App.Application.Clocks.Interfaces;
+using KlockorGrupp6App.Application.Clocks.Services;
+using KlockorGrupp6App.Infrastructure.Persistance;
+using KlockorGrupp6App.Infrastructure.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace KlockorGrupp6App.Terminal
 {
     internal class Program
     {
+        static ClockService clockService;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string connectionString;
+            
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", false);
+            var app = builder.Build();
+            connectionString = app.GetConnectionString("DefaultConnection");
+
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+
+            var context = new ApplicationContext(options);
+            IClockRepository clockRepository = new ClockRepository(context);
+            IUnitOfWork unitOfWork = new UnitOfWork(context, clockRepository);
+            clockService = new(unitOfWork);
+
+            //new ClockRepository(context)
+
+            private static async Task ListAllClocksAsync()
+        {
+            foreach (var item in await clockService.Ge)
+            {
+
+            }
+        }
         }
     }
 }
