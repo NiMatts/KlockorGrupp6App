289b2220bf7d948d53b88d1ff8c05f19f1269f20
Vilith <vilith44@gmail.com>
2025-06-12 14:45:28 +0200

    Added a collection of unit-tests and cleaned up unneccessary usings.

--- KlockorGrupp6App.Tests/Web/ClocksControllerTests.cs
+++ KlockorGrupp6App.Tests/Web/ClocksControllerTests.cs
@@ -8,11 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KlockorGrupp6App.Tests.Web
 {
